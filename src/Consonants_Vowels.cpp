e5ce185bd29397955fb4dc8fc6ca215dec37ff91
bhrgv <bhrgv@missionrnd.com>
2016-01-14 09:33:07 +0530

    Submit Commit

--- src/Consonants_Vowels.cpp
+++ src/Consonants_Vowels.cpp
@@ -18,9 +18,43 @@ NOTES: Don't create new string , Dont return anything ,you have been given two p
 */
 
 #include <stddef.h>
-
-
+int isVowel(char c)
+{
+	switch (c)
+	{
+	case 'a':
+	case 'A':
+	case 'e':
+	case 'E':
+	case 'i':
+	case 'I':
+	case 'o':
+	case 'O':
+	case 'u':
+	case 'U':
+		return 1;
+		break;
+	default:
+		return 0;
+	}
+}
 void count_vowels_and_consonants(char *str,int *consonants, int *vowels){
-
-
+	int ccount = 0;
+	int vcount = 0;
+	if (str != NULL)
+	{
+		for (int i = 0; str[i] != '\0'; i++)
+		{
+			int ascii = (int)str[i];
+			if ((ascii >= 65 && ascii <= 90) || (ascii >= 97 && ascii <= 122))
+			{
+				if (isVowel(str[i]))
+					vcount++;
+				else
+					ccount++;
+			}
+		}
+	}
+	*consonants = ccount;
+	*vowels = vcount;
 }
